80e12e505a87358bc53b41e78d33662c07540bff
VladG24 YT <vg07102004@gmail.com>
2020-06-09 10:24:13 +0300

    Update OCWTGraphics.lua

--- lib/ocwt/OCWTGraphics.lua
+++ lib/ocwt/OCWTGraphics.lua
@@ -4,76 +4,54 @@ local colors = require('colors');
 local component = require('component');
 local gpu = component.gpu;
 
-local background = colors.15, foreground = colors.0;
+local background = colors.7, foreground = colors.15; --Default background and foreground for any Component
 
+--[[
+ * One must call this function before using any OCWTGraphics' methods
+]]
 function OCWTGraphics.create(x, y, width, height)
   gpu.setBackground(background);
   gpu.setForeground(foreground);
 end
 
-function OCWTGraphics.drawImage(x, y, img)
-  
-end
-
-function function OCWTGraphics.drawLine(x1, y1, x2, y2)
+--[[
+ * Draws a straight line from point (`x1`;`y1`) to (`x2`;`y2`)
+]]
+function OCWTGraphics.drawLine(x1, y1, x2, y2)
   if gpu.getBackground()==nil or gpu.getForeground()==nil then
     error('OCWT error: OCWTGraphics not created');
   else
     local k = y1 / x1;
     local b = y1 % x1;
     coordsX = {}, coordsY = {};
-    --TODO
- --[[if x1==x2 and y1!=y2 then
-      if y1>y2 then
-        gpu.fill(x1, y2, y1-y2, 0);
-      else
-        gpu.fill(x1, y1, y2-y1, 0);
-      end
-    elseif y1==y2 and x1!=x2 then
-      if x1>x2 then
-        gpu.fill(x2, y1, 1, x1-x2);
-      else
-        gpu.fill(x1, y1, 1, x2-x1);
-      end
-    elseif (x1==y1 and x2==y2) or (x1==y2 and x2==y1) then
-      if x1>x2 then
-        for i = x2, x1, 1 do
-          gpu.fill(i, i, 1, 1);
-        end
-      else
-        for i = x1, x2, 1 do
-          gpu.fill(i, i, 1, 1);
-        end
+    if x2-x1 > y2-y1 then
+      for i = x1, x2 do
+        gpu.fill(i, k*i+b, 1, 1);
       end
     else
-      if x1>x2 and y1>y2 then
-        for i = x1, x2, 1 do
-          for j = y1, y2, 1 do
-            gpu.fill(i, j, (x1-x2)/);
-          end
-        end
-      elseif x1>x2 and y1<y2 then
-        for i = x1, x2, 1 do
-          for j = y2, y1, 1 do
-          
-          end
-        end
-      elseif x1<x2 and y1>y2 then
-        for i = x2, x1, 1 do
-          for j = y1, y2, 1 do
-          
-          end
-        end
-      elseif x1<x2 and y1<y2 then
-        for i = x2, x1, 1 do
-          for j = y2, y1, 1 do
-          
-          end
-        end
-      else
-        error('OCWTGraphics.drawLine() error: Wrong parameters');
+      for i = y1, y2 do
+        gpu.fill((i-b)/k, i, 1, 1);
       end
-    end]]
+    end
+  end
+end
+
+--[[
+ * Draws a circular arc inside rectangle at (`x`;`y`) with width `w` and height `h`
+]]
+function OCWTGraphics.drawArc(x, y, w, h --[[, startAngle, arcAngle]])
+  local ox = w/2, oy = h/2; --X and Y coordinates of circle's centre
+  if x-ox != y-oy then
+    error('OCWT error: can\'t draw non-circular arcs')
+  else
+    local radius = x-ox;
+    --if w>h then
+      for i = x, w do
+        gpu.fill(i, math.sqrt(math.power(radius, 2) - math.power(i - ox, 2)) + oy, 1, 1);
+      end
+    --elseif w<h then
+    --else
+    --end
   end
 end
 
